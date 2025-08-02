d0afd57b565fd143c6187d373ba62db27339b8b7
Donniia <da6990669@gmail.com>
2025-08-02 23:37:37 +0300

    add app layers

---
+++ android/app/src/main/kotlin/com/example/movie_cellula/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.movie_cellula
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/core/utils/assets_manager.dart
@@ -0,0 +1,14 @@
+const String imagePath ="assets/images";
+const String iconPath ="assets/icons";
+abstract class ImagesManager{
+  static const String logoRoute = "$imagePath/popcorn.png";
+}
+abstract class IconsManager{
+  static const String homeIcon = "$iconPath/homeIcon.png";
+  static const String searchIcon = "$iconPath/searchIcon.png";
+  static const String ticketIcon = "$iconPath/Ticket.png";
+  static const String clockIcon = "$iconPath/Clock.png";
+  static const String calenderIcon = "$iconPath/CalendarBlank.png";
+  static const String saveIcon = "$iconPath/saveIcon.png";
+}
+
