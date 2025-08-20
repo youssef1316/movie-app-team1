a8779b62e425140361683c4a5a6ba3f6fdeb7edb
youssef1316 <yt2301274@tkh.edu.eg>
2025-08-20 19:00:34 +0300

    Revert "merge with details fix"
    This reverts commit 4146a7be056febd27e478dcf79025e13f8437cb6.

--- lib/core/navigation/routes_manager/routes_generator.dart
+++ lib/core/navigation/routes_manager/routes_generator.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:movie_cellula/core/navigation/routes_manager/routes.dart';
-import 'package:movie_cellula/features/detail/presentation/view/detail_screen.dart';
+import 'package:movie_cellula/features/gemini/presentation/view/gemini_screen.dart';
 import 'package:movie_cellula/features/home/presentation/view/home_layout.dart';
-import '../../../features/home/domain/entities/movie.dart';
 import '../../../features/splash/presentation/splash_screen.dart';
-import 'package:movie_cellula/core/utils/app_constants.dart';
 
 
 
@@ -16,26 +14,18 @@ class RoutesGenerator {
         return MaterialPageRoute(builder: (_) => SplashScreen());
       }
 
-      case Routes.movieDetailRoute:
-        {
-          final movie = settings.arguments as Movie;
-          return MaterialPageRoute(
-            builder: (_) => DetailScreen(
-              coverImage: "https://image.tmdb.org/t/p/w500${movie.posterPath}",
-              smallImage: "https://image.tmdb.org/t/p/w500${movie.backdropPath}",
-              title: movie.title,
-              releasedate: movie.releaseDate,
-              runtime: movie.runtime,
-              rate: movie.voteAverage.toString(),
-              overview: movie.overview,
-
-            ),
-          );
-        }
+      // case Routes.movieDetailRoute:
+      //   {
+      //     return MaterialPageRoute(
+      //         builder: (_) => DetailScreen(movie: settings.arguments as Movie));
+      //   }
       case Routes.homeLayoutRoute:
         {
-          return MaterialPageRoute(
-              builder: (_) => HomeLayout());
+          return MaterialPageRoute(builder: (_) => HomeLayout());
+        }
+      case Routes.geminiRoute:
+        {
+          return MaterialPageRoute(builder: (_) => GeminiScreen());
         }
       default:
         return unDefinedRoute();
@@ -44,12 +34,7 @@ class RoutesGenerator {
 
   static Route<dynamic> unDefinedRoute() {
     return MaterialPageRoute(
-      builder: (_) => Scaffold(
-        appBar: AppBar(
-          title: const Text('No Route Found'),
-        ),
-        body: const Center(child: Text('No Route Found')),
-      ),
+      builder: (_) => HomeLayout()
     );
   }
 }
